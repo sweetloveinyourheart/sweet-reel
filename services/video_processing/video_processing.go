eb0275b548d783ef3e7c2adf1185c4d2eaaa1e7e
Nguyen Lam <sweetloveinyourheart@gmail.com>
2025-10-02 16:23:47 +0700

    refactor(video): remove unused imports and simplify InitializeRepos function

--- services/video_processing/video_processing.go
+++ services/video_processing/video_processing.go
@@ -2,25 +2,8 @@ package videoprocessing
 
 import (
 	"context"
-	"fmt"
-
-	"go.uber.org/zap"
-
-	"github.com/sweetloveinyourheart/sweet-reel/pkg/config"
-	"github.com/sweetloveinyourheart/sweet-reel/pkg/logger"
 )
 
 func InitializeRepos(ctx context.Context) error {
-	appID := fmt.Sprintf("video-processing-%s", config.Instance().GetString("video.processing.id"))
-
-	if err := InitializeCoreRepos(appID, ctx); err != nil {
-		logger.Global().ErrorContext(ctx, "failed to initialize core repos", zap.Error(err))
-		return err
-	}
-
-	return nil
-}
-
-func InitializeCoreRepos(appID string, ctx context.Context) error {
 	return nil
 }
